f2a68d035b1a9ffa4ac11076ba17ba6635db09b7
Yashibous <yashibous@gmail.com>
2024-11-13 13:24:25 +0900

    3-2 画像を一括で追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,24 @@
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Page() {
   const name = "世界";
 
   return (
-    <div>
-      <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
-      <p className={styles.description}>
-        私たちは市場をリードしているグローバルテックカンパニーです。
-      </p>
-    </div>
+    <section className={styles.top}>
+      <div>
+        <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
+        <p className={styles.description}>
+          私たちは市場をリードしているグローバルテックカンパニーです。
+        </p>
+      </div>
+      <Image
+        className={styles.bgimg}
+        src="/img-mv.jpg"
+        alt=""
+        width={4000}
+        height={1200}
+      />
+    </section>
   );
 }
