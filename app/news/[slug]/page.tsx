b6d85685543fd258aa2ba0859a8502383676fb4e
Yashibous <yashibous@gmail.com>
2024-11-18 14:04:17 +0900

    6-2

---
+++ app/news/[slug]/page.tsx
@@ -0,0 +1,22 @@
+import { notFound } from "next/navigation";
+import Article from "@/app/_components/Article";
+import ButtonLink from "@/app/_components/ButtonLink";
+import { getNewsDetail } from "@/app/_libs/microcms";
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function Page({ params }: Props) {
+  const data = await getNewsDetail(params.slug).catch(notFound);
+  return (
+    <>
+      <Article data={data} />
+      <div>
+        <ButtonLink href="/news">一覧に戻る</ButtonLink>
+      </div>
+    </>
+  );
+}
