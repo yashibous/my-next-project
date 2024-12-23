0f34d7a22974a2f8c1cf58b6784b6d578314a2d0
Yashibous <yashibous@gmail.com>
2024-12-23 15:40:00 +0900

    8-4-3

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -6,7 +6,7 @@ import { NEWS_LIST_LIMIT } from "../_constants";
 import Pagination from "@/app/_components/Pagination";
 import SearchField from "@/app/_components/SearchField";
 
-export const revalidate = 0;
+export const revalidate = 60;
 
 export default async function Page() {
   const { contents: news, totalCount } = await getNewsList({
